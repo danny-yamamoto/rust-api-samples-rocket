21705bb5b9bb586c435bc157ad31ef746bac5a27
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-12-14 11:18:37 +0000

    Add requests router path

---
+++ axum/README.md
@@ -0,0 +1,14 @@
+```bash
+curl -i localhost:8000/users/1
+```
+
+```bash
+nullvscode ➜ /workspaces/rust-api-samples-rocket (main) $ curl -i localhost:8000/users/1
+HTTP/1.1 200 OK
+content-type: application/json
+content-length: 90
+date: Thu, 14 Dec 2023 11:10:07 GMT
+
+{"user_id":1,"email_address":"maria@example.com","created_at":0,"deleted":0,"settings":""}
+vscode ➜ /workspaces/rust-api-samples-rocket (main) $ 
+```
---
+++ axum/src/routes.rs
@@ -0,0 +1,20 @@
+use std::sync::Arc;
+use axum::{Extension, extract::Path, http::StatusCode, Json};
+use sqlx::SqlitePool;
+use crate::models::{UserService, User};
+
+impl UserService {
+    pub fn new(pool: SqlitePool) -> Self {
+        UserService { pool }
+    }
+    pub async fn fetch_user(&self, user_id: i64) -> Result<Option<User>, sqlx::Error> {
+        sqlx::query_as!(User, "SELECT user_id, email_address, created_at, deleted, settings FROM users WHERE user_id = ?", user_id).fetch_optional(&self.pool).await
+    }
+}
+
+pub async fn user_handler(Path(user_id): Path<i64>, Extension(user_service): Extension<Arc<UserService>>) -> (StatusCode, Json<Option<User>>) {
+    match user_service.fetch_user(user_id).await {
+        Ok(user) => (StatusCode::OK, Json(user)),
+        Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, Json(None)),
+    }
+}
