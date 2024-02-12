1013e89793b3dc1773e65907dd05e93556f82911
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2024-02-12 07:25:49 +0000

    unwrap variables

--- rocket/src/routes.rs
+++ rocket/src/routes.rs
@@ -2,7 +2,7 @@ use crate::{get, models::UserService};
 use std::sync::Arc;
 
 use rocket::{post, serde::json::Json, State};
-use sqlx::SqlitePool;
+use sqlx::{types::chrono, SqlitePool};
 use rocket::http::{Status, ContentType};
 
 use crate::models::User;
@@ -13,16 +13,24 @@ impl UserService {
     }
 
     pub async fn fetch_user(&self, user_id: i64) -> Result<Option<User>, sqlx::Error> {
-        sqlx::query_as!(User, "SELECT user_id, email_address, created_at, deleted, settings FROM users WHERE user_id = ?", user_id)
-            .fetch_optional(&self.pool).await
+        sqlx::query_as!(
+                User, 
+                "SELECT user_id, email_address, created_at, deleted, settings FROM users WHERE user_id = ?", 
+                user_id)
+            .fetch_optional(&self.pool)
+            .await
     }
 
     pub async fn create_user(&self, user: User) -> Result<Option<User>, sqlx::Error> {
-        //let created_at = chrono::Utc::now().timestamp();
-        println!("user_id: {}", user.user_id);
-        println!("email_address: {:?}", user.email_address);
-        println!("settings: {:?}", user.settings);
-        let _res = sqlx::query!("INSERT INTO users (user_id, email_address, created_at, deleted, settings) VALUES (?, ?, ?, ?, ?)", user.user_id, "", 0, 0, "").execute(&self.pool).await?;
+        let email_address_to_insert = user.email_address.clone().unwrap_or("".to_string());
+        let created_at_to_insert = chrono::Utc::now().timestamp();
+        let settings_to_insert = user.settings.clone().unwrap_or("".to_string());
+        let _res = sqlx::query!(
+                "INSERT INTO users (user_id, email_address, created_at, deleted, settings) VALUES (?, ?, ?, ?, ?)", 
+                user.user_id, email_address_to_insert, created_at_to_insert, 0, settings_to_insert
+            )
+            .execute(&self.pool)
+            .await?;
         Ok(Some(user))
     }
 }
